a5800929f1efab69ef21e9370a9bd7c562e7286d
chorok917 <145032533+chorok917@users.noreply.github.com>
2023-09-18 16:23:07 +0900

    Add files via upload

---
+++ js/main_style.js
@@ -0,0 +1,52 @@
+AOS.init();
+
+//submenu_slide
+$(".submenu").hide();
+$(".gnb ul li").mouseenter(function () {
+    $(this).children(".submenu").stop().slideDown();
+});
+$(".gnb ul li").mouseleave(function () {
+    $(this).children(".submenu").stop().slideUp();
+});
+
+//header_scroll
+$(window).scroll(function(){
+    if($(this).scrollTop() >100){
+        $(".header_fixed").addClass("header_scroll")
+    }else{
+        $(".header_fixed").removeClass("header_scroll")
+    }
+});
+
+//top_btn
+$(".top_btn").hide();
+$(window).scroll(function(){
+    if($(this).scrollTop() >700){
+        $(".top_btn").fadeIn()
+    }else{
+        $(".top_btn").hide()
+    }
+});
+
+//item_tap_on
+$(".item_tap p").click(function(){
+    $(this).addClass("item_on").siblings().removeClass("item_on")
+});
+
+//confetti
+document.getElementsByClassName("confetti")[0].addEventListener("click", () => {
+    let canvas = document.createElement("canvas");
+    canvas.width = 600;
+    canvas.height = 600;
+    let container = document.getElementsByClassName("confetti_wrap")[0];
+    container.appendChild(canvas);
+
+    let confetti_wrap = confetti.create(canvas);
+    confetti_wrap({
+        particleCount: 200,
+        spread: 200,
+        starVelocity: 15,
+        scalar: 0.9,
+        ticks: 90
+    }).then(() => container.removeChild(canvas));
+});
